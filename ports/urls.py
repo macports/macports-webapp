61445fa190507539e1ebceb4c36eaa313714fcd1
arjunsalyan <arjunsalyan@icloud.com>
2019-04-05 22:47:19 +0530

    Add Tickets to port-detail using scrapping

--- ports/urls.py
+++ ports/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.index),
     path('search/', views.search, name='ports_search'),
+    path('load_tickets/', views.tickets, name='trac_tickets'),
     path('statistics/', views.stats, name='stats_home'),
     path('category/<slug:cat>', views.categorylist, name='category_list'),
     path('sort-by-letter/<slug:letter>', views.letterlist, name='letter_list'),
