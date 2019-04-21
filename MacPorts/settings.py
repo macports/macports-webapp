e35a6c096fa7d4de0c90c702625d9a0375b7f7be
arjunsalyan <arjunsalyan@icloud.com>
2019-04-21 20:52:59 +0530

    Add search by description

--- MacPorts/settings.py
+++ MacPorts/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'ports',
 ]
 
--- static/js/ajax-search.js
+++ static/js/ajax-search.js
@@ -1,26 +1,59 @@
 $(function () {
     $('#search').keyup(function () {
         if ($('#search').val()) {
-            $('#search-results').show()
+            $('#filtered_table').show();
+            $('#main-content').hide();
             $.ajax({
                 type: 'POST',
                 url: '/ports/search/',
                 data: {
                     'search_text': $('#search').val(),
-                     'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
+                    'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val(),
+                    'search_by': $("input[name=search_by]:checked").val(),
                 },
                 success: searchSuccess,
-                beforeSend: function() {
-                    $('#search-results').html("Searching ...")
+                beforeSend: function () {
+                    $('#filtered_table').html("Searching ...")
                 },
                 dataType: 'html'
-        });
+            });
         } else {
-            $('#search-results').hide();
+            $('#filtered_table').hide();
+            $('#main-content').show();
         }
     });
 });
 
 function searchSuccess(data, textStatus, jqXHR) {
-    $('#search-results').html(data);
+    $('#filtered_table').html(data);
+}
+
+function cancel_search() {
+    $('#filtered_table').hide();
+    $('#main-content').show();
+    $('#search').val('');
+}
+
+function switch_search() {
+    if ($('#search').val()) {
+        $('#filtered_table').show();
+        $('#main-content').hide();
+        $.ajax({
+            type: 'POST',
+            url: '/ports/search/',
+            data: {
+                'search_text': $('#search').val(),
+                'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val(),
+                'search_by': $("input[name=search_by]:checked").val(),
+            },
+            success: searchSuccess,
+            beforeSend: function () {
+                $('#filtered_table').html("Searching ...")
+            },
+            dataType: 'html'
+        });
+    } else {
+        $('#filtered_table').hide();
+        $('#main-content').show();
+    }
 }
