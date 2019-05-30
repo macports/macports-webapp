06e7c7570982164956d9fb2ee1ed44b71f28299e
arjunsalyan <arjunsalyan@icloud.com>
2019-05-30 15:01:21 +0530

    Add more test cases for parsing and fetching maintainers

--- ports/tests/test_maintainers.py
+++ ports/tests/test_maintainers.py
@@ -1,15 +1,34 @@
-from django.test import TestCase
-from ports.models import Maintainer, Port, Category, Dependency
-
+from django.test import TestCase, Client
+from django.urls import reverse
+from ports.models import Maintainer
 from parsing_scripts import load_initial_data
 
 
 class TestMaintainers(TestCase):
-    def test_unique_entries_created(self):
-        ports = load_initial_data.open_portindex_json("ports/tests/testindex.json")
+    def setUp(self):
+        ports = load_initial_data.open_portindex_json("ports/tests/sample_data/portindex.json")
         load_initial_data.load_categories_table(ports)
         load_initial_data.load_ports_and_maintainers_table(ports)
-        self.assertEquals(Maintainer.objects.count(), 5, "Failed to create unique entities for maintainers")
 
+    def test_unique_entries_created(self):
+        self.assertEquals(Maintainer.objects.count(), 6, "Failed to create unique entities for maintainers")
 
+    def test_fetch_using_github(self):
+        client = Client()
+        response = client.get(reverse('maintainer_detail_github', kwargs={'github_handle': 'user'}))
+        maintainers_returned = response.context['maintainers']
+        is_github = response.context['github']
+        num_of_ports = response.context['all_ports_num']
+        self.assertEquals(maintainers_returned.count(), 3)
+        self.assertTrue(is_github)
+        self.assertEquals(num_of_ports, 4)
 
+    def test_fetch_using_email(self):
+        client = Client()
+        response = client.get(reverse('maintainer_detail_email', kwargs={'name': 'user', 'domain': 'email.com'}))
+        maintainers_returned = response.context['maintainers']
+        is_github = response.context['github']
+        num_of_ports = response.context['all_ports_num']
+        self.assertEquals(maintainers_returned.count(), 3)
+        self.assertFalse(is_github)
+        self.assertEquals(num_of_ports, 3)
