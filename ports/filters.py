534f5e060ffed881557c5b172c0a7f9492f7d42e
arjunsalyan <arjunsalyan@icloud.com>
2019-06-10 14:27:01 +0530

    All Builds: add filter to display "All Failed Builds"

--- ports/filters.py
+++ ports/filters.py
@@ -5,10 +5,11 @@ from .models import BuildHistory, Port
 
 class BuildHistoryFilter(django_filters.FilterSet):
     port_name = django_filters.CharFilter(lookup_expr='icontains')
+    status = django_filters.CharFilter(lookup_expr='contains')
 
     class Meta:
         model = BuildHistory
-        fields = ['builder_name__name', 'status']
+        fields = ['builder_name__name']
 
 
 class PortFilterByMultiple(django_filters.FilterSet):
