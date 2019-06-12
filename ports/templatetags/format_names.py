b546a2c2806c237ea156c9b34dff2e7f61853af8
arjunsalyan <arjunsalyan@icloud.com>
2019-06-12 16:47:58 +0530

    Fix problem with stripping in name of 10.14_x86_64

--- ports/templatetags/format_names.py
+++ ports/templatetags/format_names.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter('os_version')
 def trim_builder_name(builder_name):
-    return builder_name.strip('x86_64')
+    return builder_name.replace('_x86_64', '')
