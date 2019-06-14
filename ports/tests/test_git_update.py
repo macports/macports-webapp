7f6ae61b47c186e51c4069a0266a4c1e0f85e1ad
arjunsalyan <arjunsalyan@icloud.com>
2019-06-14 12:34:41 +0530

    git-update.tests: Change path for cloning test-repo

--- ports/tests/test_git_update.py
+++ ports/tests/test_git_update.py
@@ -5,21 +5,25 @@ from django.test import TestCase
 from parsing_scripts import git_update
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEST_REPO_PARENT = os.path.join(BASE_DIR, 'tests', 'sample_data')
 
 
 class TestGitUpdates(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        git_update.clone_repo()
-
     def test_between_new_old(self):
         ports = git_update.get_list_of_changed_ports("7646d61853154b9dc523e3d0382960aea562e7ab",
-                                                        "ee0fc9c4ca59685f33628fc41c6946920869ab71")
+                                                     "ee0fc9c4ca59685f33628fc41c6946920869ab71",
+                                                     TEST_REPO_PARENT
+                                                     )
         self.assertEquals(ports, ['ideviceinstaller', 'OpenBLAS', 'nomad'])
 
     def test_between_new_and_database(self):
         git_update.get_list_of_changed_ports("7646d61853154b9dc523e3d0382960aea562e7ab",
-                                                     "ee0fc9c4ca59685f33628fc41c6946920869ab71")
+                                             "ee0fc9c4ca59685f33628fc41c6946920869ab71",
+                                             TEST_REPO_PARENT
+                                            )
 
-        ports = git_update.get_list_of_changed_ports("f3527caa506b1b944f43f47085dd35a8b8e2b050")
+        ports = git_update.get_list_of_changed_ports("f3527caa506b1b944f43f47085dd35a8b8e2b050",
+                                                     False,
+                                                     TEST_REPO_PARENT
+                                                     )
         self.assertEquals(ports, ['nomad', 'terraform'])
