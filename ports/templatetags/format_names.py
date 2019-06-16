301006de472569dca444d73b7899ea562aa82e48
arjunsalyan <arjunsalyan@icloud.com>
2019-06-16 20:04:12 +0530

    Group sub-ports on maintainer-detail

--- ports/templatetags/format_names.py
+++ ports/templatetags/format_names.py
@@ -6,3 +6,17 @@ register = template.Library()
 @register.filter('os_version')
 def trim_builder_name(builder_name):
     return builder_name.replace('_x86_64', '')
+
+
+@register.filter('split')
+def split(value, key):
+    # Returns the value turned into a list.
+    return value.split(key)
+
+
+@register.filter('index')
+def index(list, i):
+    return list[int(i)]
+
+
+
