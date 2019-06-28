c39205305c15c6c619d15414c471d0ce885490f1
arjunsalyan <arjunsalyan@icloud.com>
2019-06-28 23:22:48 +0530

    Add models for installation stats

--- ports/models.py
+++ ports/models.py
@@ -427,3 +427,60 @@ class OSDistribution(models.Model):
 class Commit(models.Model):
     hash = models.CharField(max_length=50)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class UUID(models.Model):
+    uuid = models.CharField(max_length=36, db_index=True)
+
+
+class Submission(models.Model):
+    user = models.ForeignKey(UUID, on_delete=models.CASCADE)
+    os_version = models.CharField(max_length=10)
+    xcode_version = models.CharField(max_length=10)
+    os_arch = models.CharField(max_length=20)
+    macports_version = models.CharField(max_length=10)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timestamp']),
+            models.Index(fields=['user']),
+            models.Index(fields=['user', '-timestamp']),
+        ]
+
+    @classmethod
+    def populate(cls, json_object):
+        uuid_obj, created = UUID.objects.get_or_create(uuid=json_object['id'])
+        sub = Submission()
+        sub.user = uuid_obj
+        sub.os_version = json_object['osx_version']
+        sub.xcode_version = json_object['xcode_version']
+        sub.os_arch = json_object['os_arch']
+        sub.macports_version = json_object['macports_version']
+        sub.save()
+        return
+
+
+class PortInstallation(models.Model):
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
+    port = models.CharField(max_length=100)
+    version = models.CharField(max_length=100)
+    requested = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['submission_id']),
+            models.Index(fields=['port'])
+        ]
+
+    @classmethod
+    def populate(cls, port_json, submission_id):
+        ports = []
+        for port in port_json:
+            obj = PortInstallation()
+            obj.submission_id = submission_id
+            obj.port = port['name']
+            obj.version = port['version']
+            obj.requested = True if port.get('requested') is "true" else False
+            ports.append(obj)
+        PortInstallation.objects.bulk_create(ports)
