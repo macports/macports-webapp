5ff0be6ac90fd2d5dabfa007b61480f45e0e330d
arjunsalyan <arjunsalyan@icloud.com>
2019-06-30 19:26:25 +0530

    Remove embedded url from url_generate template tag

--- ports/templatetags/url_generate.py
+++ ports/templatetags/url_generate.py
@@ -1,12 +1,14 @@
 from django import template
 register = template.Library()
 
+url_prefix = "https://build.macports.org/builders/ports-"
+
 
 @register.simple_tag
 def build_url(builder_name, build_id):
-    return "https://build.macports.org/builders/ports-" + builder_name + "-builder/builds/" + str(build_id)
+    return url_prefix + builder_name + "-builder/builds/" + str(build_id)
 
 
 @register.simple_tag
 def watcher_url(builder_name, watcher_id):
-    return "https://build.macports.org/builders/ports-" + builder_name + "-watcher/builds/" + str(watcher_id)
+    return url_prefix + builder_name + "-watcher/builds/" + str(watcher_id)
