6b3646ca9d0684cbb7682579a9d46e922afb1438
arjunsalyan <arjunsalyan@icloud.com>
2019-07-02 23:34:51 +0530

    Add total installations count in port summary

--- ports/views.py
+++ ports/views.py
@@ -105,13 +105,15 @@ def portdetail_summary(request):
 
         submissions_last_30_days = Submission.objects.filter(timestamp__gte=datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=30)).order_by('user', '-timestamp').distinct('user')
         requested_count = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_last_30_days.values('id')), requested=True, port__iexact=port_name).values('port').aggregate(Count('port'))
+        total_count = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_last_30_days.values('id')), port__iexact=port_name).values('port').aggregate(Count('port'))
 
         return render(request, 'ports/port-detail/summary.html', {
             'port': port,
             'maintainers': maintainers,
             'dependencies': dependencies,
             'variants': variants,
-            'requested_count': requested_count
+            'requested_count': requested_count,
+            'total_count': total_count
         })
     except Port.DoesNotExist:
         return HttpResponse("Visit /port/port-name/ if you are looking for ports.")
