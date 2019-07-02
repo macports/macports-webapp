da6c627f529e2340bde88aa4c4413444f4be59ce
arjunsalyan <arjunsalyan@icloud.com>
2019-07-03 00:21:09 +0530

    port-detail: Display month-wise stats in stats tab

--- ports/views.py
+++ ports/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Subquery, Count
+from django.db.models.functions import TruncMonth
 
 from .models import Port, Category, BuildHistory, Maintainer, Dependency, Builder, User, Variant, OSDistribution, Submission, PortInstallation
 from .filters import BuildHistoryFilter, PortFilterByMultiple
@@ -152,11 +153,14 @@ def portdetail_stats(request):
     submissions_last_30_days = Submission.objects.filter(timestamp__gte=datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=30)).order_by('user', '-timestamp').distinct('user')
     requested_count = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_last_30_days.values('id')), requested=True, port__iexact=port_name).values('port').aggregate(Count('port'))
     total_count = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_last_30_days.values('id')), port__iexact=port_name).values('port').aggregate(Count('port'))
-    version_distribution = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_last_30_days.values('id')), port__iexact=port_name).values('version').annotate(num=Count('version'))
+    version_distribution = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_last_30_days.values('id')), port__iexact=port_name).values('version').annotate(num=Count('version')).order_by('-num')
+
+    installation_by_month = PortInstallation.objects.filter(port__iexact=port_name).annotate(month=TruncMonth('submission__timestamp')).values('month', 'port').annotate(num=Count('submission__user', distinct=True))[:12]
     return render(request, 'ports/port-detail/installation_stats.html', {
         'requested_count': requested_count,
         'total_count': total_count,
-        'version_distribution': version_distribution
+        'version_distribution': version_distribution,
+        'installation_by_month': installation_by_month
     })
 
 
