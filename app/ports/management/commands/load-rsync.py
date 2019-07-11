8580fc608fe8e70bd60e0943c7a3a5e544e26ba4
Arjun Salyan <arjun@macports.org>
2019-07-11 20:31:17 +0530

    Add command load-rsync

---
+++ app/ports/management/commands/load-rsync.py
@@ -0,0 +1,25 @@
+import subprocess
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+
+from ports.models import Port, Commit
+
+RSYNC = "rsync://rsync.macports.org/macports//trunk/dports/PortIndex_darwin_16_i386/PortIndex.json"
+JSON_FILE = "portindex.json"
+
+
+class Command(BaseCommand):
+
+    help = "Populates the database with Initial data after fetching portindex.json from rsync"
+
+    def handle(self, *args, **options):
+        # Fetch from rsync
+        subprocess.call(['rsync', RSYNC, JSON_FILE])
+
+        # Open the file
+        with open(JSON_FILE, "r") as file:
+            data = json.load(file)
+
+        Port.load(data['ports'])
+        Commit.objects.create(hash=data['info']['commit'])
