52334f4566165f1a288a2a332f06d11eb74a4387
Arjun Salyan <arjun@macports.org>
2019-07-14 13:54:05 +0530

    Fix test_git_update.py

--- app/ports/tests/test_git_update.py
+++ app/ports/tests/test_git_update.py
@@ -3,6 +3,7 @@ import os
 from django.test import TestCase
 
 from parsing_scripts import git_update
+from ports.models import LastPortIndexUpdate
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEST_REPO_PARENT = os.path.join(BASE_DIR, 'tests', 'sample_data')
@@ -14,13 +15,15 @@ class TestGitUpdates(TestCase):
                                                      "ee0fc9c4ca59685f33628fc41c6946920869ab71",
                                                      TEST_REPO_PARENT
                                                      )
-        self.assertEquals(ports, ['ideviceinstaller', 'OpenBLAS', 'nomad'])
+        self.assertEquals(ports, ['ideviceinstaller', 'openblas', 'nomad'])
 
     def test_between_new_and_database(self):
-        git_update.get_list_of_changed_ports("7646d61853154b9dc523e3d0382960aea562e7ab",
-                                             "ee0fc9c4ca59685f33628fc41c6946920869ab71",
-                                             TEST_REPO_PARENT
-                                            )
+        if LastPortIndexUpdate.objects.count() > 0:
+            last_commit = LastPortIndexUpdate.objects.all().first()
+            last_commit.git_commit_hash = "7646d61853154b9dc523e3d0382960aea562e7ab"
+            last_commit.save()
+        else:
+            LastPortIndexUpdate.objects.create(git_commit_hash="7646d61853154b9dc523e3d0382960aea562e7ab")
 
         ports = git_update.get_list_of_changed_ports("f3527caa506b1b944f43f47085dd35a8b8e2b050",
                                                      False,
@@ -34,4 +37,4 @@ class TestGitUpdates(TestCase):
                                                      "ee0fc9c4ca59685f33628fc41c6946920869ab71",
                                                      broken_repo_parent
                                                      )
-        self.assertEquals(ports, ['ideviceinstaller', 'OpenBLAS', 'nomad'])
+        self.assertEquals(ports, ['ideviceinstaller', 'openblas', 'nomad'])
