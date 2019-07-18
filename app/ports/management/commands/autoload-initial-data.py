6bff9584e8e34207fae880b50b9bbbaccca3d38b
Arjun Salyan <arjun@macports.org>
2019-07-19 00:11:56 +0530

    autoload-initial-data: Reformat code

--- app/ports/management/commands/autoload-initial-data.py
+++ app/ports/management/commands/autoload-initial-data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from ports.models import Port, LastPortIndexUpdate
+from ports.models import Port, LastPortIndexUpdate, BuildHistory, Builder
 
 
 class Command(BaseCommand):
@@ -8,23 +8,24 @@ class Command(BaseCommand):
     help = "Populates the database with Initial data after fetching portindex.json from rsync"
 
     def handle(self, *args, **options):
-        if not Port.objects.count() > 0:
-            # Fetch from rsync
-            Port.RsyncHandler().sync()
-            # Open the file
-            data = Port.RsyncHandler().open_file()
+        if Port.objects.count() or Builder.objects.count() > 0:
+            raise CommandError("The database is not empty. Command cannot run.")
 
-            # Start populating
-            Port.load(data['ports'])
+        # Fetch from rsync
+        Port.RsyncHandler().sync()
 
-            # Write the commit hash into database
-            last_commit = LastPortIndexUpdate.objects.all().first()
-            if last_commit is None:
-                LastPortIndexUpdate.objects.create(git_commit_hash=data['info']['commit'])
-            else:
-                last_commit.git_commit_hash = data['info']['commit']
-                last_commit.save()
+        # Open the file
+        data = Port.RsyncHandler().open_file()
 
-            print("Initial Data Loaded Successfully.")
+        # Start populating
+        Port.load(data['ports'])
+
+        # Write the commit hash into database
+        last_commit = LastPortIndexUpdate.objects.all().first()
+        if last_commit is None:
+            LastPortIndexUpdate.objects.create(git_commit_hash=data['info']['commit'])
         else:
-            print("The database is not empty. Command cannot run.")
+            last_commit.git_commit_hash = data['info']['commit']
+            last_commit.save()
+
+        print("Initial Data Loaded Successfully.")
