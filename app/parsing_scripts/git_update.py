cd5df84d7373d30cd148583b15de28b7915d913c
Arjun Salyan <arjun@macports.org>
2019-07-18 22:50:15 +0530

    git_update.py: Refactor if-else block

--- app/parsing_scripts/git_update.py
+++ app/parsing_scripts/git_update.py
@@ -26,62 +26,63 @@ def clone_repo():
 
 
 def get_list_of_changed_ports(new_hash=False, old_hash=False):
-    if os.path.isdir(MACPORTS_PORTS_DIR):
-        print('{} directory found'.format(MACPORTS_PORTS))
-
-        # cd into the repository
-        os.chdir(MACPORTS_PORTS_DIR)
-
-        # Check if the repository is healthy.
-        try:
-            remote = subprocess.run([GIT, 'config', '--get', 'remote.origin.url'],
-                                    stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
-            if remote == MACPORTS_PORTS_URL:
-                print("The {} repo is healthy.".format(MACPORTS_PORTS))
-            else:
-                raise OSError
-
-            # If old_hash is not provided by the user
-            if old_hash is False:
-                # If the database has old commit, use it for old_hash
-                old_hash_object = LastPortIndexUpdate.objects.all().first()
-                if old_hash_object is None:
-                    # If database is empty, use the first commit
-                    old_hash = subprocess.run([GIT, 'rev-list', 'HEAD', '|', 'tail', '-n', '1'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-                else:
-                    old_hash = old_hash_object.git_commit_hash
-
-            # Pull from macports-ports
-            subprocess.call([GIT, 'pull'])
-
-            # Get new hash if not provided
-            if new_hash is False:
-                new_hash = subprocess.run([GIT, 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-
-            # Generate the range of commits to find updated paths
-            range = str(old_hash).strip() + "^.." + str(new_hash).strip()
-
-            changed_paths = subprocess.run([GIT, 'diff', '--name-only', range], stdout=subprocess.PIPE).stdout.decode(
-                'utf-8')
-            s = io.StringIO(changed_paths)
-            updated_ports_dir = {}
-            for line in s:
-                sections = line.split('/')
-                portdir = sections[0].lower() + '/' + sections[1].lower()
-                if portdir not in updated_ports_dir:
-                    updated_ports_dir[portdir] = ""
-            os.chdir(BASE_DIR)
-            print(updated_ports_dir)
-            return updated_ports_dir
-
-        except OSError:
-            os.chdir(DATA_DIR)
-            print("{} repository has some error".format(MACPORTS_PORTS))
-            print("Cleaning current tree and cloning new repo.")
-            shutil.rmtree(MACPORTS_PORTS_DIR)
-            clone_repo()
-            return get_list_of_changed_ports(new_hash, old_hash)
-    else:
+    # Check if the repository clone is available
+    if not os.path.isdir(MACPORTS_PORTS_DIR):
         print('{} directory not found. Cloning into'.format(MACPORTS_PORTS))
+        clone_repo()
+
+    print('{} directory found'.format(MACPORTS_PORTS))
+
+    # cd into the repository
+    os.chdir(MACPORTS_PORTS_DIR)
+
+    # Check if the repository is healthy.
+    try:
+        remote = subprocess.run([GIT, 'config', '--get', 'remote.origin.url'],
+                                stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+        if remote == MACPORTS_PORTS_URL:
+            print("The {} repo is healthy.".format(MACPORTS_PORTS))
+        else:
+            raise OSError
+
+        # If old_hash is not provided by the user
+        if old_hash is False:
+            # If the database has old commit, use it for old_hash
+            old_hash_object = LastPortIndexUpdate.objects.all().first()
+            if old_hash_object is None:
+                # If database is empty, use the first commit
+                old_hash = subprocess.run([GIT, 'rev-list', 'HEAD', '|', 'tail', '-n', '1'],
+                                          stdout=subprocess.PIPE).stdout.decode('utf-8')
+            else:
+                old_hash = old_hash_object.git_commit_hash
+
+        # Pull from macports-ports
+        subprocess.call([GIT, 'pull'])
+
+        # Get new hash if not provided
+        if new_hash is False:
+            new_hash = subprocess.run([GIT, 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+        # Generate the range of commits to find updated paths
+        range = str(old_hash).strip() + "^.." + str(new_hash).strip()
+
+        changed_paths = subprocess.run([GIT, 'diff', '--name-only', range], stdout=subprocess.PIPE).stdout.decode(
+            'utf-8')
+        s = io.StringIO(changed_paths)
+        updated_ports_dir = {}
+        for line in s:
+            sections = line.split('/')
+            portdir = sections[0].lower() + '/' + sections[1].lower()
+            if portdir not in updated_ports_dir:
+                updated_ports_dir[portdir] = ""
+        os.chdir(BASE_DIR)
+        print(updated_ports_dir)
+        return updated_ports_dir
+
+    except OSError:
+        os.chdir(DATA_DIR)
+        print("{} repository has some error".format(MACPORTS_PORTS))
+        print("Cleaning current tree and cloning new repo.")
+        shutil.rmtree(MACPORTS_PORTS_DIR)
         clone_repo()
         return get_list_of_changed_ports(new_hash, old_hash)
