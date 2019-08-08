ccdc805f807b713aae265892f9f2eaef8edbbb79
Arjun Salyan <arjun@macports.org>
2019-08-08 19:42:37 +0530

    Submission Model: Use empty values for not nullable fields

--- app/ports/models/stats.py
+++ app/ports/models/stats.py
@@ -42,19 +42,19 @@ class Submission(models.Model):
         uuid_obj, created = UUID.objects.get_or_create(uuid=json_object['id'])
         sub = Submission()
         sub.user = uuid_obj
-        os_version = json_object['os'].get('osx_version')
+        os_version = json_object['os'].get('osx_version', '')
         cxx_stdlib = json_object['os'].get('cxx_stdlib')
         if cxx_stdlib is None and LooseVersion(os_version) >= LooseVersion('10.9'):
             cxx_stdlib = "libc++"
 
         sub.os_version = os_version
-        sub.xcode_version = json_object['os'].get('xcode_version')
-        sub.os_arch = json_object['os'].get('os_arch')
-        sub.macports_version = json_object['os'].get('macports_version')
+        sub.xcode_version = json_object['os'].get('xcode_version', '')
+        sub.os_arch = json_object['os'].get('os_arch', '')
+        sub.macports_version = json_object['os'].get('macports_version', '')
         sub.cxx_stdlib = cxx_stdlib
-        sub.build_arch = json_object['os'].get('build_arch')
-        sub.platform = json_object['os'].get('os_platform')
-        sub.clt_version = json_object['os'].get('clt_version')
+        sub.build_arch = json_object['os'].get('build_arch', '')
+        sub.platform = json_object['os'].get('os_platform', '')
+        sub.clt_version = json_object['os'].get('clt_version', '')
         sub.raw_json = json_object
         sub.timestamp = timestamp
         sub.save()
@@ -83,7 +83,7 @@ class PortInstallation(models.Model):
             obj.submission_id = submission_id
             obj.port = port['name']
             obj.version = port['version']
-            obj.variants = port.get('variants')
+            obj.variants = port.get('variants', '')
             obj.requested = True if port.get('requested') == "true" else False
             ports.append(obj)
         PortInstallation.objects.bulk_create(ports)
