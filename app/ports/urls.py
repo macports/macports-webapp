9d6adfd7becc4d680da0708cd374ff080cc4c294
Arjun Salyan <arjun@macports.org>
2019-08-16 21:09:34 +0530

    Change url structure for port-detail page

--- app/ports/urls.py
+++ app/ports/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('ajax-call/summary/', views.portdetail_summary, name='port_detail_summary'),
     path('ajax-call/builds/', views.portdetail_build_information, name='port_detail_builds'),
     path('ajax-call/stats/', views.portdetail_stats, name='port_detail_stats'),
+    url(r'^(?P<name>[-a-zA-Z0-9_.]+)/(?P<slug>[a-zA-z]+)/$', views.portdetail, name='port_detail_tabbed'),
     url(r'^(?P<name>[-a-zA-Z0-9_.]+)/$', views.portdetail, name='port_detail'),
 ]
