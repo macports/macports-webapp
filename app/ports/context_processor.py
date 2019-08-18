48db1e7a8549b16de03c96e9fcb45ba32afe72ad
Arjun Salyan <arjun@macports.org>
2019-08-19 00:04:08 +0530

    Add footer

---
+++ app/ports/context_processor.py
@@ -0,0 +1,24 @@
+from ports.models import LastPortIndexUpdate, Submission, BuildHistory
+
+
+def footer_processor(request):
+    response = dict()
+    try:
+        obj = LastPortIndexUpdate.objects.all().first()
+        port_info_updated_till_commit = obj.git_commit_hash
+        port_info_updated_at = obj.updated_at
+        response['port_info_updated_till_commit'] = port_info_updated_till_commit
+        response['port_info_updated_at'] = port_info_updated_at
+    except AttributeError:
+        pass
+    try:
+        latest_submission_made_at = Submission.objects.all().order_by('-timestamp').first().timestamp
+        response['latest_submission_made_at'] = latest_submission_made_at
+    except AttributeError:
+        pass
+    try:
+        latest_build_fetched_at = BuildHistory.objects.all().order_by('-time_start').first().time_start
+        response['latest_build_fetched_at'] = latest_build_fetched_at
+    except AttributeError:
+        pass
+    return response
