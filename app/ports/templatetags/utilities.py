c37e5cff197558f7cdf73aa504902d6227b7d107
Arjun Salyan <arjun@macports.org>
2019-09-28 02:48:10 +0530

    Fix 'Most installed version' on port installation stats
    Closes: https://github.com/macports/macports-webapp/issues/123

--- app/ports/templatetags/utilities.py
+++ app/ports/templatetags/utilities.py
@@ -9,3 +9,10 @@ def find_count_for_version(distribution, version):
         if i['version'] == version:
             return i['num']
     return 0
+
+
+@register.simple_tag
+def most_installed_version(distribution):
+    sorted_dict = sorted(distribution, key=lambda x: x['num'], reverse=True)
+    expression = "{} ({} users)".format(sorted_dict[0]['version'], sorted_dict[0]['num'])
+    return expression
