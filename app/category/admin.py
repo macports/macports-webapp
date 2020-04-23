01fa17ab8e2ceb7714937194c9174a9d7b8399f4
arjunsalyan <iarjunsalyan@gmail.com>
2020-04-24 00:28:27 +0530

    Add admin site and register models

--- app/category/admin.py
+++ app/category/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from category.models import Category
+
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ("name",)
--- app/category/models.py
+++ app/category/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.TextField(primary_key=True, verbose_name="Name of the category")
 
+    def __str__(self):
+        return "%s" % self.name
+
     class Meta:
         db_table = "category"
         verbose_name = "Category"
