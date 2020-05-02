319b8787bbef039c10d8e24bfb71325351483d7f
arjunsalyan <iarjunsalyan@gmail.com>
2020-05-02 23:41:49 +0530

    Update search lookups and extend class SearchForm

--- app/port/forms.py
+++ app/port/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.utils.translation import gettext as _
-from haystack.forms import HighlightedSearchForm
+from haystack.forms import SearchForm
 from haystack.query import EmptySearchQuerySet, SearchQuerySet
 
 from port.models import Port
 
 
-class AdvancedSearch(HighlightedSearchForm):
+class AdvancedSearch(SearchForm):
     show_deleted_ports = forms.BooleanField(required=False)
     q = forms.CharField(
         required=False,
@@ -14,7 +14,7 @@ class AdvancedSearch(HighlightedSearchForm):
         widget=forms.TextInput(attrs={
             "type": "search",
             "placeholder": "Search for ports",
-            "class": "form-control",
+            "class": "form-control rounded-pill",
             "autofocus": "autofocus",
         }),
     )
@@ -37,15 +37,15 @@ class AdvancedSearch(HighlightedSearchForm):
         sqs = SearchQuerySet().models(Port).all()
 
         if self.cleaned_data['maintainers']:
-            sqs = sqs.filter_or(maintainers__search=self.cleaned_data['q'])
+            sqs = sqs.filter_or(maintainers__contains=self.cleaned_data['q'])
 
         if self.cleaned_data['name']:
-            sqs = sqs.filter_or(name__search=self.cleaned_data['q'])
+            sqs = sqs.filter_or(name__contains=self.cleaned_data["q"])
 
         if self.cleaned_data['variants']:
-            sqs = sqs.filter_or(variants__search=self.cleaned_data['q'])
+            sqs = sqs.filter_or(variants__contains=self.cleaned_data['q'])
 
         if self.cleaned_data['description']:
-            sqs = sqs.filter_or(description__search=self.cleaned_data['q'])
+            sqs = sqs.filter_or(description__contains=self.cleaned_data['q'])
 
         return sqs
