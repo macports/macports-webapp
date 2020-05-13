26fc0b9aafdc2f2a05a012806855e3d7cec9661f
arjunsalyan <iarjunsalyan@gmail.com>
2020-05-13 10:24:59 +0530

    Fix TypeError exception in version sorting

--- app/ports/utilities/sort_by_version.py
+++ app/ports/utilities/sort_by_version.py
@@ -1,9 +1,13 @@
 from distutils.version import LooseVersion
 
 
+def cleaned_version(version):
+    return version.replace("_", "")
+
+
 def sort_list_of_dicts_by_version(lst, key):
-    return sorted(lst, key=lambda x: LooseVersion(x[key]), reverse=True)
+    return sorted(lst, key=lambda x: LooseVersion(cleaned_version(x[key])), reverse=True)
 
 
 def sort_list_by_version(lst):
-    return sorted(lst, key=lambda x: LooseVersion(x), reverse=True)
+    return sorted(lst, key=lambda x: LooseVersion(cleaned_version(x)), reverse=True)
