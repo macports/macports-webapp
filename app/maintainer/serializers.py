552c803bf2f7d4260f00b5d4c240b9960ae4605e
arjunsalyan <iarjunsalyan@gmail.com>
2020-05-14 16:28:17 +0530

    Add drf view for port-detail and port-list

--- app/maintainer/serializers.py
+++ app/maintainer/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+
+from maintainer.models import Maintainer
+
+
+class MaintainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Maintainer
+        fields = ('name', 'domain', 'github')
