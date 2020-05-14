6b30333334aed66be556f05b94d82320e6098463
arjunsalyan <iarjunsalyan@gmail.com>
2020-05-14 17:22:53 +0530

    Add API call for getting categories and ports_count

---
+++ app/category/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from category.models import Category
+
+
+class CategoriesListSerializer(serializers.HyperlinkedModelSerializer):
+    ports_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ('name', 'ports_count')
+
+    def get_ports_count(self, obj):
+        return obj.ports.all().count()
