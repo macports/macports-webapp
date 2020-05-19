046a2ae29b57f2a18dd8f148dd3790674d2366d4
arjunsalyan <iarjunsalyan@gmail.com>
2020-05-19 20:41:16 +0530

    Store and display livecheck error also

--- app/parsing_scripts/run_livecheck.py
+++ app/parsing_scripts/run_livecheck.py
@@ -6,34 +6,33 @@ from port.models import Port, LiveCheck
 
 def run_livecheck_all():
     for port in Port.objects.all().only('id', 'name'):
-        try:
-            output = subprocess.run(["port", "livecheck", port.name], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        except OSError:
-            continue
-        obj, created = LiveCheck.objects.get_or_create(port_id=port.id)
-        obj.result = None
-        lines = io.StringIO(output)
-        for line in lines:
-            if "seems to have been updated" in line:
-                obj.result = line
-        obj.save()
+        run_livecheck_port(port)
     return
 
 
-def run_livecheck_port(name):
+def run_livecheck_port(port):
     try:
-        port = Port.objects.get(name=name)
-    except Port.DoesNotExist:
-        return
-    try:
-        output = subprocess.run(["port", "livecheck", port.name], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        output = subprocess.run(["port", "livecheck", port.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = output.stdout.decode('utf-8')
+        error = output.stderr.decode('utf-8')
     except OSError:
         return
-    lines = io.StringIO(output)
+    result_lines = io.StringIO(result)
+    error_lines = io.StringIO(error)
+
+    # Get or create the Livecheck object and initialise values
     obj, created = LiveCheck.objects.get_or_create(port_id=port.id)
     obj.result = None
-    for line in lines:
-        if "seems to have been updated" in line:
+    obj.error = None
+    obj.has_updates = False
+
+    for line in result_lines:
+        if line.startswith(port.name + " seems to"):
             obj.result = line
+            obj.has_updates = True
+
+    for line in error_lines:
+        if line.startswith("Error:"):
+            obj.error = line
     obj.save()
     return
