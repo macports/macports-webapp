6f0baaae694436d2fda84b4c7219ea665e5d85f3
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-03 00:13:33 +0530

    Add general stats api endpoint

--- app/stats/views.py
+++ app/stats/views.py
@@ -13,7 +13,7 @@ from rest_framework.views import APIView
 from stats.models import Submission, PortInstallation
 from stats.validators import validate_stats_days, validate_columns_port_installations, validate_unique_columns_port_installations, ALLOWED_DAYS_FOR_STATS
 from stats.utilities.sort_by_version import sort_list_of_dicts_by_version
-from stats.serializers import PortStatisticsSerializer, PortMonthlyInstallationsSerializer
+from stats.serializers import PortStatisticsSerializer, PortMonthlyInstallationsSerializer, GeneralStatisticsSerializer
 
 
 def stats(request):
@@ -208,6 +208,20 @@ class PortMonthlyInstallationsAPIView(APIView):
             PortInstallation.objects.all(),
             context={
                 'name': request.GET.get('name'),
+            }
+        )
+        return Response(result.data)
+
+
+class GeneralStatisticsAPIView(APIView):
+    def get(self, request):
+        result = GeneralStatisticsSerializer(
+            Submission.objects.all(),
+            context={
+                'days': request.GET.get('days', "30"),
+                'days_ago': request.GET.get('days_ago', "0"),
+                'property': request.GET.getlist('property'),
+                'sort_by': request.GET.get('sort_by')
             }
         )
         return Response(result.data)
