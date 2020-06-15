af9d9fd5681899f1232a00467bda858d1c6e19e1
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-15 13:26:02 +0530

    API: display list of port names in maintainer detail view

--- app/maintainer/serializers.py
+++ app/maintainer/serializers.py
@@ -3,10 +3,30 @@ from rest_framework import serializers
 from maintainer.models import Maintainer
 
 
-class MaintainerSerializer(serializers.ModelSerializer):
+class MaintainerListSerializer(serializers.ModelSerializer):
+    ports_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Maintainer
-        fields = ('name', 'domain', 'github')
+        fields = ('name', 'domain', 'github', 'ports_count')
+
+    def get_ports_count(self, obj):
+        return obj.ports.all().count()
+
+
+class MaintainerDetailSerializer(serializers.ModelSerializer):
+    ports_count = serializers.SerializerMethodField()
+    ports = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Maintainer
+        fields = ('name', 'domain', 'github', 'ports_count', 'ports')
+
+    def get_ports_count(self, obj):
+        return obj.ports.all().count()
+
+    def get_ports(self, obj):
+        return obj.ports.filter(active=True).all().values_list('name', flat=True)
 
 
 class MaintainerHaystackSerializer(serializers.Serializer):
