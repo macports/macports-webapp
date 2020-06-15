28176594a562aeb581dd716574c8ff7ad5004b4d
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-15 13:54:11 +0530

    Fix port info api lookup regex

--- app/port/views.py
+++ app/port/views.py
@@ -157,7 +157,7 @@ class PortInfoView(viewsets.ReadOnlyModelViewSet):
     serializer_class = PortSerializer
     queryset = Port.objects.all()
     lookup_field = 'name__iexact'
-    lookup_value_regex = '[a-zA-Z0-9_.]+'
+    lookup_value_regex = '[a-zA-Z0-9_.-]+'
     filter_backends = [filters.SearchFilter, django_filters.rest_framework.DjangoFilterBackend]
     search_fields = ['name', 'maintainers__github', 'variants__variant', 'categories__name']
     filterset_fields = ['name', 'categories', 'maintainers__github', 'variants__variant']
