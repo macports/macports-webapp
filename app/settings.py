4f0605db8ef8e514b0bac5b2e08dce66ff90bed2
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-22 21:02:04 +0530

    Add smtp email configuration

--- app/settings.py
+++ app/settings.py
@@ -198,3 +198,11 @@ CACHES = {
       'LOCATION': '127.0.0.1:11211',
    }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
