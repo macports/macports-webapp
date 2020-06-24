b552e1917972cd4f3f6c3cb26fc61c2aa31b4df8
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-24 17:31:27 +0530

    Add redirect for old port summary page

--- app/port/urls.py
+++ app/port/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from rest_framework import routers
+from django.views.generic.base import RedirectView
 
 from port import views
 
@@ -13,4 +14,7 @@ urlpatterns = [
     url(r'^(?P<name>[-a-zA-Z0-9_.]+)/stats/$', views.port_detail_stats, name='port_stats'),
     url(r'^(?P<name>[-a-zA-Z0-9_.]+)/tickets/$', views.port_detail_tickets, name='port_tickets'),
     url(r'^(?P<name>[-a-zA-Z0-9_.]+)/$', views.port_detail, name='port_detail'),
+
+    # redirect for the old summary url
+    url(r'^(?P<name>[-a-zA-Z0-9_.]+)/summary/$', RedirectView.as_view(pattern_name='port_detail'), name='port_summary'),
 ]
