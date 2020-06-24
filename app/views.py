21e14f102fca8ab9fde08cde395da8a6a78dcbe9
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-24 12:28:19 +0530

    Update homepage and logo

--- app/views.py
+++ app/views.py
@@ -1,22 +1,21 @@
 import datetime
 
 from django.shortcuts import render
-from django.db.models import Subquery, Count, Q
+from django.db.models import Subquery, Count
 
-from category.models import Category
 from port.models import Port
 from stats.models import Submission, PortInstallation
 from port.filters import PortFilterByMultiple
 
 
 def index(request):
-    categories = Category.objects.all().order_by('name').annotate(ports_count=Count('ports', filter=Q(ports__active=True)))
+    ports_count = Port.objects.filter(active=True).count()
     submissions_unique = Submission.objects.filter(timestamp__gte=datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=30)).order_by('user', '-timestamp').distinct('user')
     top_ports = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_unique.values('id')), requested=True).exclude(port__icontains='mpstats').values('port').annotate(num=Count('port')).order_by('-num')[:10]
 
     return render(request, 'index.html', {
-        'categories': categories,
-        'top_ports': top_ports
+        'top_ports': top_ports,
+        'ports_count': ports_count
     })
 
 
