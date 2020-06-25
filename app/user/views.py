30de1d017cd80d936f2452805aa23bdd7b1b0095
arjunsalyan <iarjunsalyan@gmail.com>
2020-06-25 15:47:27 +0530

    Add 'maintained by me' account view

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from user.utilities import get_my_ports_context
 
 
 def profile(request):
     return render(request, 'account/profile.html')
+
+
+@login_required
+def my_ports_github(request):
+    handles, emails, ports_github, ports_email, ports = get_my_ports_context(request, 'GitHub')
+
+    return render(request, 'account/my_ports.html', {
+        'using': 'GitHub',
+        'connections': handles,
+        'ports_by_email_count': ports_email,
+        'ports_by_github_count': ports_github,
+        'ports': ports
+    })
+
+
+@login_required
+def my_ports_email(request):
+    handles, emails, ports_github, ports_email, ports = get_my_ports_context(request, 'email')
+
+    return render(request, 'account/my_ports.html', {
+        'using': 'email',
+        'connections': emails,
+        'ports_by_email_count': ports_email,
+        'ports_by_github_count': ports_github,
+        'ports': ports
+    })
