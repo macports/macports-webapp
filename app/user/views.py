715eda7ee0ef3a32b7760abfafea94535272e567
arjunsalyan <iarjunsalyan@gmail.com>
2020-07-08 20:53:36 +0530

    My ports: Add filters and latest build

--- app/user/views.py
+++ app/user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from user.utilities import get_my_ports_context
+from user.forms import MyPortsForm
 
 
 def profile(request):
@@ -10,25 +11,29 @@ def profile(request):
 
 @login_required
 def my_ports_github(request):
-    handles, emails, ports_github, ports_email, ports = get_my_ports_context(request, 'GitHub')
+    handles, emails, ports_github, ports_email, ports, builder = get_my_ports_context(request, 'GitHub')
 
     return render(request, 'account/my_ports.html', {
         'using': 'GitHub',
         'connections': handles,
         'ports_by_email_count': ports_email,
         'ports_by_github_count': ports_github,
-        'ports': ports
+        'ports': ports,
+        'builder': builder,
+        'form': MyPortsForm(request.GET)
     })
 
 
 @login_required
 def my_ports_email(request):
-    handles, emails, ports_github, ports_email, ports = get_my_ports_context(request, 'email')
+    handles, emails, ports_github, ports_email, ports, builder = get_my_ports_context(request, 'email')
 
     return render(request, 'account/my_ports.html', {
         'using': 'email',
         'connections': emails,
         'ports_by_email_count': ports_email,
         'ports_by_github_count': ports_github,
-        'ports': ports
+        'ports': ports,
+        'builder': builder,
+        'form': MyPortsForm(request.GET)
     })
