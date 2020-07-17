1eaf88463c2585bba5413ad226db041415cd9b20
arjunsalyan <iarjunsalyan@gmail.com>
2020-07-17 15:07:28 +0530

    Add experimental dark mode

--- app/user/urls.py
+++ app/user/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('my_ports/email/', views.my_ports_email, name='my_ports_email'),
     path('followed_ports/', views.followed_ports, name='followed_ports'),
     path('notifications/', views.notifications_all, name='notifications'),
-    path('notifications/unread/', views.notifications_all, name='notifications')
+    path('notifications/unread/', views.notifications_all, name='notifications_unread')
 ]
