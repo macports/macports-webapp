4ba267b5497a2bb304f41e367ac9943f906cfb13
arjunsalyan <iarjunsalyan@gmail.com>
2020-07-21 12:31:58 +0530

    Port details: Display subports

--- app/port/utilities.py
+++ app/port/utilities.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseRedirect
 
+from port.models import Port
+
 
 def redirect_back(request, default_uri):
     redirect_uri = request.META.get('HTTP_REFERER')
@@ -7,3 +9,18 @@ def redirect_back(request, default_uri):
         return HttpResponseRedirect(redirect_uri)
     else:
         return HttpResponseRedirect(default_uri)
+
+
+def get_subports(port, portdir):
+    subports = None
+
+    portdir_splitted = portdir.split('/')
+    if len(portdir_splitted) < 2:
+        return subports
+
+    port_name = portdir_splitted[1]
+
+    if port == port_name:
+        subports = Port.objects.filter(portdir__iexact=portdir).exclude(name=port_name).order_by('name')
+
+    return subports
