c8d7bc8204a546a641146e2970e58b80aa27cd9a
arjunsalyan <iarjunsalyan@gmail.com>
2020-08-02 23:18:41 +0530

    Model Variant: Add conflicts, requires and is_default fields

--- app/variant/models.py
+++ app/variant/models.py
@@ -6,6 +6,9 @@ class Variant(models.Model):
     port = models.ForeignKey('port.Port', on_delete=models.CASCADE, related_name='variants')
     variant = models.CharField(max_length=100, default='')
     description = models.TextField(null=True)
+    requires = models.TextField(null=True)
+    conflicts = models.TextField(null=True)
+    is_default = models.BooleanField(default=False)
 
     class Meta:
         db_table = "variant"
