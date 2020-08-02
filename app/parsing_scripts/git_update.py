b4f2cb2e0cb5a7f3888b590cc3e3d0f55730f115
arjunsalyan <iarjunsalyan@gmail.com>
2020-08-02 23:53:38 +0530

    update-portinfo: Update notes and variant description also

--- app/parsing_scripts/git_update.py
+++ app/parsing_scripts/git_update.py
@@ -56,7 +56,7 @@ def refresh_portindex_json():
     latest_commit = subprocess.run([config.GIT, 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     # update/generate the portindex
-    subprocess.run(['portindex', '-p', 'macosx_19_i386'])
+    subprocess.run(['portindex', '-p', 'macosx_19_i386', '-x'])
 
     # update/generate portindex.json
     portindexjson = subprocess.run([config.TCLSH, config.PORTINDEX2JSON, config.LOCAL_PORTINDEX, '--info', 'commit={}'.format(latest_commit)], stdout=subprocess.PIPE).stdout.decode('utf-8')
