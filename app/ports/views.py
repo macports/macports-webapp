4d9328de2f8de224d662a7ca9a690ccd7abbba2d
Arjun Salyan <arjun@macports.org>
2021-03-28 00:24:59 +0530

    Update landing page UI

--- app/ports/views.py
+++ app/ports/views.py
@@ -20,13 +20,15 @@ from .utilities.sort_by_version import sort_list_of_dicts_by_version
 
 
 def index(request):
-    categories = Category.objects.all().order_by('name').annotate(ports_count=Count('category', filter=Q(category__active=True)))
+    #categories = Category.objects.all().order_by('name').annotate(ports_count=Count('category', filter=Q(category__active=True)))
+    all_ports_count = Port.get_active.all().count()
     submissions_unique = Submission.objects.filter(timestamp__gte=datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=30)).order_by('user', '-timestamp').distinct('user')
     top_ports = PortInstallation.objects.filter(submission_id__in=Subquery(submissions_unique.values('id')), requested=True).exclude(port__icontains='mpstats').values('port').annotate(num=Count('port')).order_by('-num')[:10]
 
     return render(request, 'ports/index.html', {
-        'categories': categories,
-        'top_ports': top_ports
+        #'categories': categories,
+        'top_ports': top_ports,
+        'all_ports_count': all_ports_count
     })
 
 
