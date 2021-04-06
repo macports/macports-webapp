a8064f936682b7b1fe01938fe91f474217c94dd6
Arjun Salyan <arjun@macports.org>
2021-04-06 17:14:33 +0000

    Remove unescaped variables from HttpResponse

--- app/ports/validators.py
+++ app/ports/validators.py
@@ -9,21 +9,21 @@ def validate_stats_days(value):
     try:
         value = int(value)
         if value not in ALLOWED_DAYS_FOR_STATS:
-            return False, "'{}' is an invalid value. Allowed values are: {}".format(value, ALLOWED_DAYS_FOR_STATS)
+            return False, "Invalid value supplied. Allowed values are: {}".format(ALLOWED_DAYS_FOR_STATS)
     except ValueError:
-        return False, "Received '{}'. Expecting an integer.".format(value)
+        return False, "Invalid value supplied. Expecting an integer."
     return True, "Validation Successful"
 
 
 def validate_columns_port_installations(columns):
     for value in columns:
         if value not in ALLOWED_COLUMNS_FOR_SORT:
-            return False, "{} is an invalid column. Expecting: {}".format(value, ALLOWED_COLUMNS_FOR_SORT)
+            return False, "Invalid column value. Expecting: {}".format(ALLOWED_COLUMNS_FOR_SORT)
     return True, "Validation Successful"
 
 
 def validate_unique_columns_port_installations(values):
     for i, j in itertools.combinations(values, 2):
         if i.replace('-', '') == j.replace('-', ''):
-            return False, "'{}' and '{}' refer to the same column.".format(i, j)
+            return False, "Duplicate columns supplied. Expecting unique values for each column."
     return True, "Validation Successful"
