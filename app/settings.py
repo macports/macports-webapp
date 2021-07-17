d1987f4ce44ad916c509387492b69868fde66b72
Arjun Salyan <arjun@macports.org>
2021-07-17 20:23:42 +0530

    Set DEBUG=False in production

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
