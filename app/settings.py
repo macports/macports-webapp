fd5a5c68c7d76aa0bd7b71eb102a38c38b3ec53c
Arjun Salyan <arjun@macports.org>
2021-07-17 19:18:54 +0530

    Add config for setting "from email"

--- app/settings.py
+++ app/settings.py
@@ -225,6 +225,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL', 'no-reply@ports.macports.org')
 
 
 DJANGO_NOTIFICATIONS_CONFIG = {
