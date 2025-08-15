c1fe93675acc5b5a14b534aa3be10fa65c41b4bd
Joshua Root <jmr@macports.org>
2025-08-16 07:36:53 +1000

    Change MemcachedCache to PyMemcacheCache

--- app/settings.py
+++ app/settings.py
@@ -214,7 +214,7 @@ REST_FRAMEWORK = {
 
 CACHES = {
    'default': {
-      'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+      'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
       'LOCATION': '127.0.0.1:11211',
    }
 }
