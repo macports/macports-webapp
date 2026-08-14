2a450810035913c14c9a22e3fc861c9fa0879b86
Dmitry Marakasov <474217+AMDmi3@users.noreply.github.com>
2026-08-15 02:08:44 +0300

    Include port version and revision in build history

--- app/buildhistory/serializers.py
+++ app/buildhistory/serializers.py
@@ -14,7 +14,7 @@ class BuildHistorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BuildHistory
-        fields = ('port_name', 'builder_name', 'build_id', 'status', 'time_start', 'time_elapsed', 'watcher_id')
+        fields = ('port_name', 'port_version', 'port_revision', 'builder_name', 'build_id', 'status', 'time_start', 'time_elapsed', 'watcher_id')
 
 
 class FilesSerializer(serializers.ModelSerializer):
